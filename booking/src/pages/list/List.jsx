41af5021d055d2dca6a0db5dcba28057156c7f8f
Chawatvish Worrapoj <chawatvish.w@kbtg.tech>
2022-08-15 22:05:49 +0700

    Added header support for list page

--- booking/src/pages/list/List.jsx
+++ booking/src/pages/list/List.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import NavBar from '../../components/navbar/Navbar'
+import Header from '../../components/header/Header'
 
 const List = () => {
   return (
-    <div>List</div>
+    <div>
+      <NavBar />
+      <Header type="list"/>
+    </div>
   )
 }
 
